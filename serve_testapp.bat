abc72548d27df1f7327af7dcc938ba8de1faadf4
vcm <zl311@duke.edu>
2022-02-05 11:56:54 -0500

    update

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\PycharmProjects\testapp
+cd C:\Users\vcm\testapp
 git pull https://%TESTAPP_GIT_PAT%@github.com/zeshengli98/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
