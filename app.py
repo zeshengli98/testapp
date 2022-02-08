22f3706d92a9e9a268e9d8ee3e5aa394dfc9af3d
vcm <zl311@duke.edu>
2022-02-08 18:56:01 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/cute_ferret.jpeg")
+    html.Img(src="assets/cute_ferret.jpeg"),
+    html.Div(children='done well!!!'),
 ])
 
 if __name__ == '__main__':
